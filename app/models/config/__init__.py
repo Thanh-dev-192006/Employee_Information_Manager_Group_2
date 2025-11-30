06ca497099a2bba40791984279c5e69a64f6a6a3
Thanh Trinh <lauriel00789@gmail.com>
2025-12-01 00:24:20 +0700

    Fix import issues

---
+++ app/models/config/__init__.py
@@ -0,0 +1,10 @@
+
+from ..manager.employee import EmployeeManager
+from ..manager.department import DepartmentManager
+from ..manager.project import ProjectManager
+from ..manager.assignment import AssignmentManager
+from ..manager.attendance import AttendanceManager
+from ..manager.salary import SalaryManager
+from ..manager.bonus_deduction import BonusDeductionManager
+from ..manager.query import QueryManager
+
