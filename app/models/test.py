015f1148a5a0f966197816691e16a9e56c302ea7
Thanh Trinh <lauriel00789@gmail.com>
2025-12-03 21:54:56 +0700

    Commit

--- app/models/test.py
+++ app/models/test.py
@@ -1,4 +1,4 @@
-from employee_manager import EmployeeManager
+from Employee_Information_Manager_Group_2.app.models.manager.employee import EmployeeManager
 
 result = EmployeeManager.get_all_employees()
 print(result)
